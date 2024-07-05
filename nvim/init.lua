4b41d2dcc9b318ce7428a97a7d9021d1bfa89b89
Andrei Stebakov <lispercat@gmail.com>
2024-07-05 18:21:08 -0400

    set modifiable bug

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.cmd("set modifiable")
 --Write file to disk upon change
 vim.api.nvim_create_autocmd({
   "InsertLeave",
