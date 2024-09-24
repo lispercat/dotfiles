61bcccbce70678f092e80919551d6dc02ee372ec
Andrei Stebakov <lispercat@gmail.com>
2024-09-24 16:22:02 -0400

    remove redundant stuff

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -15,53 +15,6 @@ return {
     },
   },
   {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      local treesitter = require("nvim-treesitter.configs")
-
-      -- configure treesitter
-      treesitter.setup({ -- enable syntax highlighting
-        highlight = {
-          enable = true,
-        },
-        -- enable indentation
-        indent = { enable = true },
-        -- ensure these language parsers are installed
-        ensure_installed = {
-          "json",
-          "javascript",
-          "typescript",
-          "tsx",
-          "yaml",
-          "html",
-          "css",
-          "prisma",
-          "markdown",
-          "markdown_inline",
-          "svelte",
-          "graphql",
-          "bash",
-          "lua",
-          "vim",
-          "dockerfile",
-          "gitignore",
-          "query",
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<C-space>",
-            node_incremental = "<C-space>",
-            scope_incremental = false,
-            node_decremental = "<bs>",
-          },
-        },
-      })
-    end,
-  },
-  {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
@@ -85,15 +38,6 @@ return {
           never_show = {},
         },
       },
-    },
-  },
-  {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "nvim-neotest/nvim-nio",
-      "nvim-lua/plenary.nvim",
-      "nvim-neotest/neotest-python",
-      "nvim-treesitter/nvim-treesitter",
     },
   },
   {
@@ -165,9 +109,4 @@ return {
         -- refer to the configuration section below
     }
   }
-  -- {
-  --   'dccsillag/magma-nvim',
-  --   build = ':UpdateRemotePlugins',
-  --   lazy = false,  -- or true, depending on when you want the plugin to load
-  -- }
 }
