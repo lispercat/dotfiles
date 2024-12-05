ac1a5f725c267432d802a165621e6d59939f4b20
Andrei Stebakov <lispercat@gmail.com>
2024-12-05 18:14:30 -0500

    added YankAssasin to keep curor on the last line of selection

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -104,9 +104,21 @@ return {
   {
     "gbprod/substitute.nvim",
     opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-    }
-  }
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+  {
+    "svban/YankAssassin.nvim",
+    config = function()
+      require("YankAssassin").setup({
+        auto_normal = true, -- if true, autocmds are used. Whenever y is used in normal mode, the cursor doesn't move to start
+        auto_visual = true, -- if true, autocmds are used. Whenever y is used in visual mode, the cursor doesn't move to start
+      })
+      -- Optional Mappings
+      vim.keymap.set({ "x", "n" }, "gy", "<Plug>(YADefault)", { silent = true })
+      vim.keymap.set({ "x", "n" }, "<leader>y", "<Plug>(YANoMove)", { silent = true })
+    end,
+  },
 }
