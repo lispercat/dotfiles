23054cb09a33ab9bc1320087f23d66a24b383a03
Andrei Stebakov <lispercat@gmail.com>
2025-02-22 15:26:23 -0500

    Prevented Ctrl-l from clearing CopilotChat window

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,10 +1,10 @@
 return {
-    {
+  {
     "github/copilot.vim",
-      config = function()
+    config = function()
       vim.g.copilot_assume_mapped = true
       vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-    end
+    end,
   },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
@@ -17,6 +17,19 @@ return {
     opts = {
       debug = true,
     },
+    config = function()
+      require("CopilotChat").setup({})
+
+      -- Add autocommand to disable <C-l> in CopilotChat buffer
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "copilot-chat",
+        callback = function()
+          vim.defer_fn(function()
+            vim.api.nvim_buf_set_keymap(0, "n", "<C-l>", "<Nop>", { noremap = true, silent = true })
+          end, 100) -- Delay by 100ms to ensure plugin setup is complete
+        end,
+      })
+    end,
   },
   {
     "zbirenbaum/copilot-cmp",
@@ -29,33 +42,33 @@ return {
       cmp.setup({
         snippet = {
           expand = function(args)
-            require('luasnip').lsp_expand(args.body)
+            require("luasnip").lsp_expand(args.body)
           end,
         },
         mapping = {
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          ['<Tab>'] = cmp.mapping(function(fallback)
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
-            elseif require('luasnip').expand_or_jumpable() then
-              require('luasnip').expand_or_jump()
+            elseif require("luasnip").expand_or_jumpable() then
+              require("luasnip").expand_or_jump()
             else
               fallback()
             end
-          end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
-            elseif require('luasnip').jumpable(-1) then
-              require('luasnip').jump(-1)
+            elseif require("luasnip").jumpable(-1) then
+              require("luasnip").jump(-1)
             else
               fallback()
             end
-          end, { 'i', 's' }),
+          end, { "i", "s" }),
         },
         sources = {
           { name = "copilot" },
@@ -77,7 +90,7 @@ return {
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
-      "hrsh7th/cmp-nvim-lsp",  -- LSP completion source
+      "hrsh7th/cmp-nvim-lsp", -- LSP completion source
     },
     config = function()
       -- Import nvim-cmp
@@ -92,14 +105,13 @@ return {
           end,
         },
         mapping = {
-          ['<C-Space>'] = cmp.mapping.complete(),  -- Trigger completion
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),  -- Confirm selection
+          ["<C-Space>"] = cmp.mapping.complete(), -- Trigger completion
+          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Confirm selection
         },
         sources = {
-          { name = "nvim_lsp" },  -- LSP completions
+          { name = "nvim_lsp" }, -- LSP completions
         },
       })
-    end
-  }
+    end,
+  },
 }
-
