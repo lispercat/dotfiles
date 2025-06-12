d9e0ae2124f4582c737aff125f8c58e89ef39f6b
Andrei Stebakov <andrei_stebakov@otpp.com>
2025-06-12 10:25:03 -0400

    update wezterm.lua on Windows

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,11 +5,13 @@ local act = wezterm.action
 local config = wezterm.config_builder()
 config.color_scheme = 'Catppuccin Mocha'
 
+-- Session manager integration
 local session_manager = require("wezterm-session-manager/session-manager")
 wezterm.on("save_session", function(window) session_manager.save_state(window) end)
 wezterm.on("load_session", function(window) session_manager.load_state(window) end)
 wezterm.on("restore_session", function(window) session_manager.restore_state(window) end)
 
+-- Key bindings
 config.keys = {
   { key = "t", mods = "CTRL|SHIFT", action = act.SpawnCommandInNewTab { args = { "wsl" } } },
   { key = "S", mods = "LEADER", action = wezterm.action{EmitEvent = "save_session"} },
@@ -17,6 +19,8 @@ config.keys = {
   { key = "R", mods = "LEADER", action = wezterm.action{EmitEvent = "restore_session"} },
   { key = 'v', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
 }
+
+-- WSL domain setup
 config.wsl_domains = {
   {
     name = "WSL:Ubuntu",
